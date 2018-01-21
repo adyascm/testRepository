0d25120d9e5388b299dd526518ae9af65366f7a0
Amit Agarwal <amit@Amits-MBP.Dlink>
2018-01-21 08:49:27 +0530

    Correcting the unittest

--- api/adya/tests/authTests.py
+++ api/adya/tests/authTests.py
@@ -3,7 +3,7 @@ from api.adya.datasources.google import authProvider
 
 class RegistrationTestCase(unittest.TestCase):
     def test_registration(self):
-        self.assertEqual(authProvider.login("amit","amit","abc123"),{"email":"amit"})
+        self.assertNotEqual(authProvider.login_request(), "")
 
 if __name__ == '__main__':
     unittest.main()
