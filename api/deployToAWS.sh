cb23832b09a9a9e5094cc442e45255c82efcdeb0
Amit Agarwal <amit@Amits-MBP.Dlink>
2018-01-21 09:27:56 +0530

    Checking jenkins trigger 3

--- api/deployToAWS.sh
+++ api/deployToAWS.sh
@@ -7,8 +7,7 @@ mkdir target/dist &> /dev/null
 mkdir target/dist/adyaapp &> /dev/null
 
 cp -r adya target/dist/adyaapp
-mv target/dist/adyaapp/adya/lib/lib/python2.7/site-packages/* target/dist/adyaapp
-rm -rf target/dist/adyaapp/adya/lib
+mv lib/lib/python2.7/site-packages/* target/dist/adyaapp
 
 cp serverless.yml target/dist/adyaapp/
 cd target/dist/adyaapp
