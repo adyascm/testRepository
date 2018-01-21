58176f6e35203c87613bc957f4c055bb688ffdbd
Amit Agarwal <amit@Amits-MBP.Dlink>
2018-01-21 08:55:47 +0530

    Testing jenkins trigger 1

--- api/adya/tests/authTests.py
+++ api/adya/tests/authTests.py
@@ -1,6 +1,7 @@
 import unittest
 from api.adya.datasources.google import authProvider
 
+#Testing jenkins trigger
 class RegistrationTestCase(unittest.TestCase):
     def test_registration(self):
         self.assertNotEqual(authProvider.login_request(), "")
