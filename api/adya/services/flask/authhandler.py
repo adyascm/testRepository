090d02cd9f6eee90523e4c4f58d8ef8e74df6591
Rashmi Singh <rashmi.singh@adya.io>
2018-01-24 11:33:48 +0530

    feat(store domain information into db): while authzcallback store domain information and login user

--- api/adya/services/flask/authhandler.py
+++ api/adya/services/flask/authhandler.py
@@ -1,5 +1,8 @@
+import urlparse
+
 from flask_restful import Resource,reqparse,request
 from adya.datasources.google import authProvider
+
 
 class googleoauthlogin(Resource):
     def get(self):
@@ -12,6 +15,10 @@ class googleoauthcallback(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('error', type=str, help='Error if oauth failed')
         args = parser.parse_args()
+        print "url ", req.url
+        # parsed = urlparse.urlparse(req.url)
+        # code = urlparse.parse_qs(parsed.query)['code']
+        # print "code ", code
         url = authProvider.login_callback(req.url, args['error'])
         if not url:
             return {'message': 'Not authenticated'}, 401
