91960b9a2feaa56db89edf290efb8f53307355e2
Rashmi Singh <rashmi.singh@adya.io>
2018-01-24 15:29:40 +0530

    feat(check for GSuite verified domain):

--- api/adya/common/constants.py
+++ api/adya/common/constants.py
@@ -1,5 +1,5 @@
 import os
 
-AAPI_HOST = os.environ.get('API_HOST', 'http://localhost:5000')
+API_HOST = os.environ.get('API_HOST', 'http://localhost:5000')
 REDIRECT_STATUS = os.environ.get('REDIRECT_STATUS', 'http://localhost:3000/success')
 
