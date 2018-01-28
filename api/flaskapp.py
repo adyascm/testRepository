1d84cc1366bffa530137a5133c3a6d8349348487
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2018-01-28 11:07:18 +0530

    Missied in last checkin

--- api/flaskapp.py
+++ api/flaskapp.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from adya.services.flask.authhandler import googleoauthlogin,googleoauthcallback
-from adya.services.flask.scanhandler import initialDSScan,processResources,getPermission
+from adya.services.flask.scanhandler import initialgdrivescan,processResources,getPermission,getdomainuser,gdriveScan
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -12,9 +12,11 @@ api = Api(app)
 #Add all routes here
 api.add_resource(googleoauthlogin, '/googleoauthlogin')
 api.add_resource(googleoauthcallback, '/googleoauthcallback')
-api.add_resource(initialDSScan,'/gdrivescan')
+api.add_resource(gdriveScan,'/gdrivescan')
 api.add_resource(processResources,'/processresources')
 api.add_resource(getPermission,'/permisssions')
+api.add_resource(getdomainuser,'/getdomainuser')
+api.add_resource(initialgdrivescan,'/initialgdrivescan')
 
 if __name__ == '__main__':
     app.run(debug=True)
