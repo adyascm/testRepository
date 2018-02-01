576f2552d985674a3e3b2141e6f6af4193749589
Rashmi Singh <rashmi.singh@adya.io>
2018-02-01 14:46:16 +0530

    fix(incremental authorization scopes):

--- api/adya/common/constants.py
+++ api/adya/common/constants.py
@@ -1,7 +1,6 @@
 import os
 from enum import Enum
 
-
 API_HOST = os.environ.get('API_HOST', 'http://localhost:5000')
 UI_HOST = os.environ.get('UI_HOST', 'http://localhost:3000')
 
@@ -16,7 +15,7 @@ GOOGLE_OAUTH_CALLBACK_URL = API_HOST + GOOGLE_OAUTHCALLBACK_PATH
 OAUTH_STATUS_PATH = "/oauthstatus"
 OAUTH_STATUS_URL = UI_HOST + OAUTH_STATUS_PATH
 
-NEXT_CALL_FROM_FILE_ID = 4*60
+NEXT_CALL_FROM_FILE_ID = 4 * 60
 
 GDRIVE_SCAN_PATH = "/scan/gdrivescan"
 GDRIVE_SCAN_URL = API_HOST + GDRIVE_SCAN_PATH
@@ -34,7 +33,6 @@ GET_DOMAIN_USER_URL = API_HOST + GET_DOMAIN_USER_PATH
 PROCESS_USERS_PATH = "/scan/processusers"
 PROCESS_USERS_DATA_URL = API_HOST + PROCESS_USERS_PATH
 
-
 GET_DOMAIN_GROUP_PATH = "/scan/getdomaingroups"
 GET_GROUP_URL = API_HOST + GET_DOMAIN_GROUP_PATH
 PROCESS_GROUP_PATH = "/scan/processgroups"
@@ -47,15 +45,19 @@ PROCESS_GROUP_MEMBER_DATA_URL = API_HOST + PROCESS_GROUP_MEMBER_PATH
 GET_DATASOURCE_URL = '/getdatasources'
 
 PROFILE_VIEW_SCOPE = ['profile '
-                       'email ' ]
+                      'email ']
 
-READ_DRIVE_SCOPE = ['https://www.googleapis.com/auth/drive.readonly ']
+READ_DRIVE_SCOPE = ['profile '
+                    'email '
+                    'https://www.googleapis.com/auth/drive.readonly ']
 
-FULL_SCOPE = ['https://www.googleapis.com/auth/drive '
-          'https://www.googleapis.com/auth/admin.directory.user '
-          'https://www.googleapis.com/auth/admin.directory.group '
-          'https://www.googleapis.com/auth/admin.reports.audit.readonly '
-          'https://www.googleapis.com/auth/drive ']
+FULL_SCOPE = ['profile '
+              'email '
+              'https://www.googleapis.com/auth/drive '
+              'https://www.googleapis.com/auth/admin.directory.user '
+              'https://www.googleapis.com/auth/admin.directory.group '
+              'https://www.googleapis.com/auth/admin.reports.audit.readonly '
+              'https://www.googleapis.com/auth/drive ']
 
 SCOPE_DICT = {
     "profile_view": PROFILE_VIEW_SCOPE,
@@ -73,13 +75,14 @@ class ResourceExposureType(Enum):
 
 
 class PermissionType(Enum):
-      READ ="R"
-      WRITE ="W"
+    READ = "R"
+    WRITE = "W"
 
 
 class UserMemberType(Enum):
     INTERNAL = "INT"
     EXTERNAL = "EXT"
+
 
 class GroupMemberType(Enum):
     INTERNAL = "INT"
