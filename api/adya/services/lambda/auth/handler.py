39f5f30dbe3478a1a17321573ae97376dd2ebb3c
Amit Agarwal <amit@Amits-MacBook-Pro.local>
2018-02-05 17:00:01 +0530

    Adding the dump for context also

--- api/adya/services/lambda/auth/handler.py
+++ api/adya/services/lambda/auth/handler.py
@@ -24,10 +24,15 @@ def google_oauth_request(event, context):
 
 
 def google_oauth_callback(event, context):
+    print(json.dumps(event))
+    print(json.dumps(context))
+    params_dict = event["queryStringParameters"]
     auth_code = event["queryStringParameters"]["code"]
-    error = event["queryStringParameters"]["error"]
-    scope = event["queryStringParameters"]["scope"]
-    auth_url = auth.oauth_callback(auth_code, scope, error)
+    error_msg = ""
+    if error in params_dict:
+        error_msg = params_dict["error"]
+    scope = params_dict["scope"]
+    auth_url = auth.oauth_callback(auth_code, scope, error_msg)
     if not auth_url:
         response = {
             "statusCode": 301,
