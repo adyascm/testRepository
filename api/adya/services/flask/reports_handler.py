16fca60283be59bb065a4a6847d1bf435114ca2d
Rashmi Singh <rashmi.singh@adya.io>
2018-02-07 11:46:20 +0530

    feat(create report api):

--- api/adya/services/flask/reports_handler.py
+++ api/adya/services/flask/reports_handler.py
@@ -13,3 +13,14 @@ class dashboard_widget(Resource):
         widget_id = request.args["widgetId"]
         data = reports_controller.get_widget_data(auth_token, widget_id)
         return data, 200
+
+
+class scheduled_report(Resource):
+    def post(self):
+        auth_token = request.headers.get('Authorization')
+        if not auth_token:
+            return {'message': 'Missing auth token'}, 400
+        payload = request.get_json()
+        datasource = reports_controller.create_report(auth_token,payload)
+
+        return datasource, 201
