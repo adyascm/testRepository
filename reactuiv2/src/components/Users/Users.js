d5e0cd6f4bd076b64080ef539cf7c0b48d60939e
Amit Agarwal <amit@Amits-MacBook-Pro.local>
2018-02-07 17:24:44 +0530

    Binding the users tree to the api and some refactoring

--- reactuiv2/src/components/Users/Users.js
+++ reactuiv2/src/components/Users/Users.js
@@ -16,6 +16,7 @@ import {
 } from '../../constants/actionTypes';
 import UserAccess from './UserAccess';
 import UserDetails from './UserDetails';
+import UsersTree from './UsersTree';
 
 const mapStateToProps = state => ({
   appName: state.common.appName,
@@ -32,106 +33,6 @@ const mapDispatchToProps = dispatch => ({
 class Users extends Component {
   constructor(props) {
     super(props);
-    this.treeConfig = {
-      stateful: false,
-      stateKey: 'tree-grid-1',
-      gridType: 'tree', // either `tree` or `grid`,
-      showTreeRootNode: true, // dont display root node of tree
-      columns: [
-        {
-          dataIndex: 'category',
-          name: 'Category',
-          expandable: true // this will be the column that shows the nested hierarchy
-        }
-      ],
-      plugins: {},
-    };
-    this.treeData = { root: { id: 1, parentId: -1, category: 'Category 1', children: [{ id: 2, parentId: 1, category: 'Category 2', children: [] }] } }, { id: 3, parentId: -1, category: 'Category 1', children: [{ id: 4, parentId: 3, category: 'Category 2', children: [] }] };
-
-    this.state = {
-      columnDefs: [
-        {
-          headerName: "Group",
-          field: "group",
-          cellRenderer: "agGroupCellRenderer"
-        },
-        {
-          headerName: "Last Active",
-          field: "last_active"
-        }
-      ],
-      rowData: [
-        {
-          group: "Group A",
-          participants: [
-            {
-              group: "A.1",
-              last_active: "10 mins",
-            },
-            {
-              group: "A.2",
-              last_active: "10 mins",
-            },
-            {
-              group: "A.3",
-              last_active: "10 mins",
-            }
-          ]
-        },
-        {
-          group: "Group B",
-          last_active: "10 mins",
-          participants: [
-            {
-              group: "B.1",
-              last_active: "10 mins",
-            },
-            {
-              group: "B.2",
-              last_active: "10 mins",
-            },
-            {
-              group: "B.3",
-              last_active: "10 mins",
-            },
-            {
-              group: "B.4",
-              last_active: "10 mins",
-            },
-            {
-              group: "B.5",
-              last_active: "10 mins",
-            }
-          ]
-        },
-        {
-          group: "Group C",
-          last_active: "10 mins",
-          participants: [
-            {
-              group: "C.1",
-              last_active: "10 mins",
-            },
-            {
-              group: "C.2",
-              last_active: "10 mins",
-            }
-          ]
-        }
-      ],
-      getNodeChildDetails: function getNodeChildDetails(rowItem) {
-        if (rowItem.participants) {
-          return {
-            group: true,
-            expanded: rowItem.group === "Group C",
-            children: rowItem.participants,
-            key: rowItem.group
-          };
-        } else {
-          return null;
-        }
-      }
-    };
 
     this.panes = [
       { menuItem: 'Details', render: () => <Tab.Pane attached={false}><UserDetails /></Tab.Pane> },
@@ -154,16 +55,7 @@ class Users extends Component {
       <Grid stretched celled='internally'>
         <Grid.Row>
           <Grid.Column width={7}>
-            {/* <TreeGrid { ...this.treeConfig } data={this.treeData}/> */}
-            <div className="ag-theme-fresh">
-              <AgGridReact
-                id="myGrid" domLayout="autoHeight"
-                columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData}
-                getNodeChildDetails={this.state.getNodeChildDetails}
-                onGridReady={this.onGridReady.bind(this)}
-              />
-            </div>
+            <UsersTree />
           </Grid.Column>
           <Grid.Column width={9}>
           <Container fluid >
