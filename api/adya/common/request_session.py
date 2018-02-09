2dbf31eb3d985fcded3c59ac97fd444986a88d29
Amit Agarwal <amit@Amits-MBP.Dlink>
2018-02-09 11:15:35 +0530

    Flask vs Lambda check did not work, so trying a different way

--- api/adya/common/request_session.py
+++ api/adya/common/request_session.py
@@ -13,16 +13,16 @@ class RequestSession():
     def validate_authorized_request(self, validateAuth=True, mandatory_params=[], optional_params=[]):
         #Validate the flask request
         params_dict = {}
-        if type(self.req) is type(request):
-            print("Request is understood as a flask request")
-            self.auth_token = self.req.headers.get('Authorization')
-            params_dict = self.req.args
-        #Validate the lambda event object
-        else:
+        if self.req['requestContext'] and self.req['requestContext']['accountId']:
             print("Request is understood as a lambda request")
             self.isLocal = False
             self.auth_token = self.req["headers"]["Authorization"]
             params_dict = self.req["queryStringParameters"]
+        #Validate the lambda event object
+        else:
+            print("Request is understood as a flask request")
+            self.auth_token = self.req.headers.get('Authorization')
+            params_dict = self.req.args
 
         if validateAuth and not self.auth_token:
                 return self.generate_error_response(401, "Not authenticated")
