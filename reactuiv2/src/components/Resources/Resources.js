b82baffd1e0388dd4db043e86e923b0a0b26ae08
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-02-09 20:45:49 +0530

    Code Refactor for Resources

--- reactuiv2/src/components/Resources/Resources.js
+++ reactuiv2/src/components/Resources/Resources.js
@@ -3,17 +3,14 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
-import { Grid, Image, Tab, Container } from 'semantic-ui-react'
-
-import { AgGridReact } from "ag-grid-react";
-import 'ag-grid/dist/styles/ag-grid.css';
-import 'ag-grid/dist/styles/ag-theme-fresh.css';
+import { Grid, Container } from 'semantic-ui-react'
 
 import {
   RESOURCES_PAGE_LOADED,
   RESOURCES_PAGE_UNLOADED
 } from '../../constants/actionTypes';
-import ResourcePermissions from './ResourcePermissions';
+import ResourcesTree from './ResourcesTree';
+import ResourcePermissionSection from './ResourcePermissionSection';
 
 import agent from '../../utils/agent';
 
@@ -32,136 +29,7 @@ const mapDispatchToProps = dispatch => ({
 class Users extends Component {
   constructor(props) {
     super(props);
-    this.treeConfig = {
-      stateful: false,
-      stateKey: 'tree-grid-1',
-      gridType: 'tree', // either `tree` or `grid`,
-      showTreeRootNode: true, // dont display root node of tree
-      columns: [
-        {
-          dataIndex: 'category',
-          name: 'Category',
-          expandable: true // this will be the column that shows the nested hierarchy
-        }
-      ],
-      plugins: {},
-    };
-    this.treeData = { root: { id: 1, parentId: -1, category: 'Category 1', children: [{ id: 2, parentId: 1, category: 'Category 2', children: [] }] } }, { id: 3, parentId: -1, category: 'Category 1', children: [{ id: 4, parentId: 3, category: 'Category 2', children: [] }] };
-
-    this.state = {
-      columnDefs: [
-        {
-            headerName: "Resource",
-            field: "group",
-            cellRenderer: "agGroupCellRenderer"
-        },
-        {
-            headerName: "Owner",
-            field: "owner"
-        }
-    ],
-    rowData: [
-        {
-            group: "Folder A",
-            owner: "amit@adya.io",
-            permission: "write",
-            participants: [
-                {
-                    group: "A.1",
-                    owner: "amit@adya.io",
-            permission: "write",
-                },
-                {
-                    group: "A.2",
-                    owner: "tinkesh@adya.io",
-            permission: "read",
-                },
-                {
-                    group: "A.3",
-                    owner: "rashmi@adya.io",
-            permission: "write",
-                }
-            ]
-        },
-        {
-            group: "Folder B",
-            owner: "deepak@adya.io",
-            permission: "read",
-            participants: [
-                {
-                    group: "B.1",
-                    owner: "amit@adya.io",
-            permission: "read",
-                },
-                {
-                    group: "B.2",
-                    owner: "tinkesh@adya.io",
-            permission: "read",
-                },
-                {
-                    group: "B.3",
-                    owner: "amit@adya.io",
-            permission: "write",
-                },
-                {
-                    group: "B.4",
-                    owner: "abhra@adya.io",
-            permission: "write",
-                },
-                {
-                    group: "B.5",
-                    owner: "amit@adya.io",
-            permission: "write",
-                }
-            ]
-        },
-        {
-            group: "Folder C",
-            owner: "amit@adya.io",
-            permission: "write",
-            participants: [
-                {
-                    group: "C.1",
-                    owner: "amit@adya.io",
-            permission: "write",
-                },
-                {
-                    group: "C.2",
-                    owner: "deepak@adya.io",
-            permission: "read",
-                }
-            ]
-        }
-    ],
-    getNodeChildDetails: function getNodeChildDetails(rowItem) {
-        if (rowItem.participants) {
-            return {
-                group: true,
-                expanded: false,
-                children: rowItem.participants,
-                key: rowItem.group
-            };
-        } else {
-            return null;
-        }
-    }
-    };
-
-    this.panes = [
-      { menuItem: 'Permissions', render: () => <Tab.Pane attached={false}><ResourcePermissions /></Tab.Pane> },
-      
-    ];
   }
-  onGridReady(params) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-
-    params.api.sizeColumnsToFit();
-  }
-
-//   componentWillMount() {
-//     console.log("resources tree : ", agent.Resources.getResourcesTree())
-//   }
 
   render() {
     let containerStyle = {
@@ -171,20 +39,11 @@ class Users extends Component {
       <Grid stretched celled='internally'>
         <Grid.Row>
           <Grid.Column width={7}>
-            <div className="ag-theme-fresh">
-              <AgGridReact
-                id="myGrid" domLayout="autoHeight"
-                rowSelection='single' suppressCellSelection='true'
-                columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData}
-                getNodeChildDetails={this.state.getNodeChildDetails}
-                onGridReady={this.onGridReady.bind(this)}
-              />
-            </div>
+            <ResourcesTree />
           </Grid.Column>
           <Grid.Column width={9}>
             <Container fluid >
-                <Tab menu={{ secondary: true, pointing: true }} panes={this.panes} />
+                <ResourcePermissionSection />
             </Container>
           </Grid.Column>
         </Grid.Row>
