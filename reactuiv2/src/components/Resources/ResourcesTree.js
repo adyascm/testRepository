8f71d05428180d5c4af09d979cb6452f214aadf1
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-02-12 20:05:13 +0530

    Rendering resources from api

--- reactuiv2/src/components/Resources/ResourcesTree.js
+++ reactuiv2/src/components/Resources/ResourcesTree.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import {Loader,Dimmer} from 'semantic-ui-react';
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid/dist/styles/ag-grid.css';
 import 'ag-grid/dist/styles/ag-theme-fresh.css';
 
 import agent from '../../utils/agent';
 import ResourceCell from './ResourceCell';
-import {RESOURCES_PAGE_LOADED} from '../../constants/actionTypes';
+import {RESOURCES_PAGE_LOADED, RESOURCES_PAGE_LOAD_START} from '../../constants/actionTypes';
 
 
 const mapStateToProps = state => ({
@@ -15,6 +16,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
+   onLoadStart: () => dispatch({type:RESOURCES_PAGE_LOAD_START}),
    onLoad: (payload) => dispatch({type:RESOURCES_PAGE_LOADED,payload})
 });
 
@@ -23,94 +25,20 @@ class ResourcesTree extends Component {
         super(props);
 
         this.cellExpanded = this.cellExpanded.bind(this);
+        //this.getTreeRows = this.getTreeRows.bind(this);
         
         this.state = {
+            resourceTree: '',
             columnDefs: [
               {
                   headerName: "Resource",
-                  field: "group",
+                  field: "name",
+                  cellStyle: {textAlign: "left"},
                   //cellRenderer: "agGroupCellRenderer",
                   cellRendererFramework: ResourceCell,
                   cellRendererParams: {
                     cellExpanded: this.cellExpanded
                   }
-              },
-              {
-                  headerName: "Owner",
-                  field: "owner"
-              }
-          ],
-          rowData: [
-              {
-                  group: "Folder A",
-                  owner: "amit@adya.io",
-                  permission: "write",
-                  participants: [
-                      {
-                          group: "A.1",
-                          owner: "amit@adya.io",
-                  permission: "write",
-                      },
-                      {
-                          group: "A.2",
-                          owner: "tinkesh@adya.io",
-                  permission: "read",
-                      },
-                      {
-                          group: "A.3",
-                          owner: "rashmi@adya.io",
-                  permission: "write",
-                      }
-                  ]
-              },
-              {
-                  group: "Folder B",
-                  owner: "deepak@adya.io",
-                  permission: "read",
-                  participants: [
-                      {
-                          group: "B.1",
-                          owner: "amit@adya.io",
-                  permission: "read",
-                      },
-                      {
-                          group: "B.2",
-                          owner: "tinkesh@adya.io",
-                  permission: "read",
-                      },
-                      {
-                          group: "B.3",
-                          owner: "amit@adya.io",
-                  permission: "write",
-                      },
-                      {
-                          group: "B.4",
-                          owner: "abhra@adya.io",
-                  permission: "write",
-                      },
-                      {
-                          group: "B.5",
-                          owner: "amit@adya.io",
-                  permission: "write",
-                      }
-                  ]
-              },
-              {
-                  group: "Folder C",
-                  owner: "amit@adya.io",
-                  permission: "write",
-                  participants: [
-                      {
-                          group: "C.1",
-                          owner: "amit@adya.io",
-                  permission: "write",
-                      },
-                      {
-                          group: "C.2",
-                          owner: "deepak@adya.io",
-                  permission: "read",
-                      }
-                  ]
               }
           ],
           getNodeChildDetails: function getNodeChildDetails(rowItem) {
@@ -138,6 +66,25 @@ class ResourcesTree extends Component {
 
     }
 
+    getTreeRows() {
+        let rows = [];
+        //console.log("resourceTree keys : ", this.props.resourceTree)
+        let resourceTreeData = this.props.resourceTree
+        if (resourceTreeData) {
+            let datasourceId = Object.keys(resourceTreeData)
+            resourceTreeData = resourceTreeData[datasourceId]
+            var keys = Object.keys(resourceTreeData)
+
+            for (let index=0; index<keys.length; index++) {
+                let row = resourceTreeData[keys[index]]
+                if (!row.name)
+                    row.name = row.resourceName
+                rows.push(row)
+            }
+        }
+        return rows;
+    }
+
     onGridReady(params) {
         this.gridApi = params.api;
         this.gridColumnApi = params.columnApi;
@@ -146,17 +93,38 @@ class ResourcesTree extends Component {
     }
     
     componentWillMount() {
-        console.log("resources tree : ", agent.Resources.getResourcesTree({}))
+        this.props.onLoadStart()
+        this.props.onLoad(agent.Resources.getResourcesTree({}))
+        this.setState({
+            resourceTree: agent.Resources.getResourcesTree({})
+        })
+
     }
 
     render() {
+        if (this.props.resourceTree !== undefined) {
+            let resourceTreeData = this.props.resourceTree
+            let datasourceId = Object.keys(resourceTreeData)
+            console.log("resource Tree : ", this.props.resourceTree[datasourceId])
+        }
+        if (!this.props.resourceTree) {
+            if (this.props.isLoading) {
+                return (
+                    <div className="ag-theme-fresh" style={{ height: '200px' }}>
+                        <Dimmer active inverted>
+                            <Loader inverted content='Loading' />
+                        </Dimmer>
+                    </div>
+                )
+            }
+        }
         return (
             <div className="ag-theme-fresh">
               <AgGridReact
                 id="myGrid" domLayout="autoHeight"
                 rowSelection='single' suppressCellSelection='true'
                 columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData}
+                rowData={this.getTreeRows()}
                 getNodeChildDetails={this.state.getNodeChildDetails}
                 onGridReady={this.onGridReady.bind(this)}
                 //gridOptions={this.gridOptions}
--- reactuiv2/src/reducers/resources.js
+++ reactuiv2/src/reducers/resources.js
@@ -1,10 +1,16 @@
 import {
     RESOURCES_PAGE_LOADED,
-    RESOURCES_PAGE_UNLOADED
+    RESOURCES_PAGE_UNLOADED,
+    RESOURCES_PAGE_LOAD_START
 } from '../constants/actionTypes';
 
 export default (state={},action) => {
     switch(action.type) {
+        case RESOURCES_PAGE_LOAD_START:
+            return {
+                ...state,
+                isLoading: true
+            }
         case RESOURCES_PAGE_LOADED:
             return {
                 ...state,
