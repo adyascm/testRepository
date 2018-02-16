d93b7e5018615aac2226d3967cce71d84bc1e6c4
Sumit kumar <tinkesh.kumar@adya.io>
2018-02-16 20:36:27 +0530

    Modified for getting parent from resourceParent table

--- api/adya/controllers/resourceController.py
+++ api/adya/controllers/resourceController.py
@@ -1,5 +1,5 @@
 from adya.db.connection import db_connection
-from adya.db.models import Resource,ResourcePermission,LoginUser,DataSource,ResourcePermission
+from adya.db.models import Resource,ResourcePermission,LoginUser,DataSource,ResourcePermission,ResourceParent
 from sqlalchemy import and_
 import json
 from adya.common import utils
@@ -18,34 +18,44 @@ def get_resource_tree(auth_token, parent_id,emailList=None):
             resource_permissions_query_data=None
             if not parent_id:
                 resource_permissions_query_data = db_session.query(Resource,ResourcePermission).outerjoin(ResourcePermission,
-                                        and_(ResourcePermission.resource_id == Resource.resource_id,
-                                        ResourcePermission.domain_id == Resource.domain_id)).filter(and_(Resource.domain_id == domain_id,
-                                        Resource.resource_type =='folder',
+                                        and_(ResourcePermission.resource_id == Resource.resource_id,\
+                                        ResourcePermission.domain_id == Resource.domain_id))\
+                                        .filter(and_(Resource.domain_id == domain_id,\
+                                        Resource.resource_type =='folder',\
                                         ResourcePermission.email.in_(emailList))).all()
             else:
                 resource_permissions_query_data = db_session.query(Resource,ResourcePermission).outerjoin(ResourcePermission,
                                         and_(ResourcePermission.resource_id == Resource.resource_id,
-                                        ResourcePermission.domain_id == Resource.domain_id)).filter(and_(Resource.domain_id == domain_id,
-                                        ResourcePermission.email.in_(emailList),Resource.resource_parent_id == parent_id)).all()
+                                        ResourcePermission.domain_id == Resource.domain_id))\
+                                        .outerjoin(ResourceParent,and_(ResourceParent.resource_id == Resource.resource_id,\
+                                        ResourceParent.domain_id == Resource.domain_id))\
+                                        .filter(and_(Resource.domain_id == domain_id,
+                                        ResourcePermission.email.in_(emailList),ResourceParent.resource_parent_id == parent_id)).all()
             resource_id_set = Set()
             for row in resource_permissions_query_data:
                 resource_id_set.add(row.Resource.resource_id)
             resource_data =[]
             for row in resource_permissions_query_data:
-                if (row.Resource.resource_parent_id != None and not row.Resource.resource_parent_id in resource_id_set) \
-                    or row.Resource.resource_parent_id == None :
+                if (row.ResourceParent.resource_parent_id != None and not row.ResourceParent.resource_parent_id in resource_id_set) \
+                    or row.ResourceParent.resource_parent_id == None :
                     resources.append(row)
         else:
             if not parent_id:
                 resources = db_session.query(Resource,ResourcePermission).outerjoin(ResourcePermission,
                                     and_(ResourcePermission.resource_id == Resource.resource_id,
-                                    ResourcePermission.domain_id == Resource.domain_id)).filter(and_(Resource.domain_id == domain_id, 
-                                    Resource.resource_type =='folder',Resource.resource_parent_id == parent_id)).all()
+                                    ResourcePermission.domain_id == Resource.domain_id))
+                                    .outerjoin(ResourceParent,and_(ResourceParent.resource_id == Resource.resource_id,\
+                                        ResourceParent.domain_id == Resource.domain_id))\
+                                    .filter(and_(Resource.domain_id == domain_id, 
+                                    Resource.resource_type =='folder',ResourceParent.resource_parent_id == parent_id)).all()
             else:
                 resources = db_session.query(Resource,ResourcePermission).outerjoin(ResourcePermission,
                                     and_(ResourcePermission.resource_id == Resource.resource_id,
-                                    ResourcePermission.domain_id == Resource.domain_id)).filter(and_(Resource.domain_id == domain_id, 
-                                    Resource.resource_parent_id == parent_id)).all()
+                                    ResourcePermission.domain_id == Resource.domain_id))
+                                    .outerjoin(ResourceParent,and_(ResourceParent.resource_id == Resource.resource_id,\
+                                    ResourceParent.domain_id == Resource.domain_id))\
+                                    .filter(and_(Resource.domain_id == domain_id,\ 
+                                    ResourceParent.resource_parent_id == parent_id)).all()
         
         responsedata ={}
         for resource in resources:
