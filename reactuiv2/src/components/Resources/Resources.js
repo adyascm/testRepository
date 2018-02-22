b2d7e3fc27a375df3dde99f4a1de217a041227d4
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-02-22 15:03:00 +0530

    DropDown option to filter files by File Exposure type in Resources Tab including api change

--- reactuiv2/src/components/Resources/Resources.js
+++ reactuiv2/src/components/Resources/Resources.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-import { Grid, Container } from 'semantic-ui-react'
+import { Grid, Container, Dropdown, Form } from 'semantic-ui-react'
 
 import ResourcesList from './ResourcesList';
 import ResourcePermissionSection from './ResourcePermissionSection';
 import ResourcesActions from '../actions/ResourcesActions';
 
+import {RESOURCES_SET_FILE_SHARE_TYPE} from '../../constants/actionTypes';
+
 const mapStateToProps = state => ({
     ...state.resources
+});
+
+const mapDispatchToProps = dispatch => ({
+  setFileExposureType: (payload) => dispatch({ type: RESOURCES_SET_FILE_SHARE_TYPE, payload })
 });
 
 
 class Users extends Component {
   constructor(props) {
     super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      options: [
+        {text: 'External Shared',
+         value: 'External Shared'},
+        {text: 'Domain Shared',
+         value: 'Domain Shared'},
+        {text: 'Internally Shared',
+         value: 'Internally Shared'}
+      ],
+      fileExposureType: {
+        'External Shared': 'EXT',
+        'Domain Shared': 'DOMAIN',
+        'Internally Shared': 'INT'
+      }
+    }
+  }
+
+  handleChange(event,data) {
+    this.props.setFileExposureType(this.state.fileExposureType[data.value])
   }
 
   render() {
@@ -29,7 +57,24 @@ class Users extends Component {
 
     return (
       <Container style={containerStyle}>
-        <Grid stretched >
+        <Grid divided='vertically' stretched >
+          <Grid.Row >
+            <Grid.Column stretched width="5">
+              <Dropdown
+                options={this.state.options}
+                selection
+                onChange={this.handleChange}
+                defaultValue="External Shared"
+              />
+            </Grid.Column>
+            <Grid.Column stretched width="5">
+              <Form>
+                <Form.Field>
+                  <input placeholder='Filter by File type ...' />
+                </Form.Field>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
           <Grid.Row stretched>
             <Grid.Column stretched width={gridWidth}>
               <ResourcesList />
@@ -50,4 +95,4 @@ class Users extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Users);
+export default connect(mapStateToProps,mapDispatchToProps)(Users);
