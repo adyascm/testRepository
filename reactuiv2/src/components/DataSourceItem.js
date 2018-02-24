81ce16d6b25a9ab44d11bfe48a38b776928f4725
Amit Agarwal <amit@Amits-MBP.Dlink>
2018-02-24 17:19:32 +0530

    Show the sync and scan status on the UI

--- reactuiv2/src/components/DataSourceItem.js
+++ reactuiv2/src/components/DataSourceItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Image, Dimmer, Segment, Loader, Progress } from 'semantic-ui-react'
+import { Button, Card, Image, Dimmer, Segment, Loader, Progress, Label } from 'semantic-ui-react'
 import agent from '../utils/agent';
 
 
@@ -17,13 +17,27 @@ const DataSourceItem = props => {
         });
     }
 
-    if (datasource) {
-        var percent = ((datasource.processed_file_count/datasource.total_file_count)*100)
-        if(datasource.total_file_count == 0)
+    const getScanStatus = (datasource) => {
+        var percent = ((datasource.processed_file_count / datasource.total_file_count) * 100)
+        if (datasource.total_file_count == 0)
             percent = 0;
+
+        if (datasource.file_scan_status > 1 || datasource.user_scan_status > 1 || datasource.group_scan_status > 1)
+            return <Progress size='small' precision='0' percent={percent} error />; //Failed
+
+        if ((datasource.file_scan_status == 1 && datasource.total_file_count == datasource.processed_file_count) && (datasource.user_scan_status == 1 && datasource.total_user_count == datasource.processed_user_count) && (datasource.group_scan_status == 1 && datasource.total_group_count == datasource.processed_group_count))
+            return <Progress size='small' precision='0' percent={percent} success />; //Complete
+        return <Progress size='small' precision='0' percent={percent} active />; //In Progress
+    }
+
+    if (datasource) {
         var statusText = "Processed " + datasource.processed_file_count + "/" + datasource.total_file_count + " files " + datasource.processed_group_count + "/" + datasource.total_group_count + " groups " + datasource.processed_user_count + "/" + datasource.total_user_count + " users"
+        var status = getScanStatus(datasource);
+        var syncStatus = <Label style={{ marginLeft: "5px" }} circular color='red' key='red'>Sync Disabled</Label>;
+        if (datasource.is_push_notifications_enabled)
+            syncStatus = <Label style={{ marginLeft: "5px" }} circular color='green' key='green'>Syncing</Label>;
         return (
-            <Card fluid>
+            <Card fluid >
                 <Dimmer active={datasource.isDeleting} inverted>
                     <Loader inverted content='Deleting...' />
                 </Dimmer>
@@ -31,13 +45,14 @@ const DataSourceItem = props => {
                     <Image floated='left' size='small' src='/images/GSuite.png' />
                     <Card.Header textAlign='right'>
                         {datasource.display_name}
+                        {syncStatus}
                     </Card.Header>
                     <Card.Meta textAlign='right'>
                         Created at: <strong>{datasource.creation_time}</strong>
                     </Card.Meta>
                     <Card.Description>
                         {statusText}
-                        <Progress size='small' precision='0'  percent={percent} />
+                        {status}
                     </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
