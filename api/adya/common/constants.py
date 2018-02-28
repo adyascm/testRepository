0c272070272a8b9575f7dc4b009d02e8c5a50a65
Rashmi Singh <rashmi.singh@adya.io>
2018-02-28 15:21:51 +0530

    code refactor

--- api/adya/common/constants.py
+++ api/adya/common/constants.py
@@ -1,8 +1,6 @@
 import os
 from enum import Enum
 
-from adya.common.aws_utils import get_lambda_name
-
 SERVERLESS_SERVICE_NAME = 'adya'
 DEPLOYMENT_ENV = os.environ.get('DEPLOYMENT_ENV', 'local')
 
@@ -17,9 +15,6 @@ DB_URL = os.environ.get('DB_URL', 'localhost:3306')
 DB_USERNAME = os.environ.get('DB_USERNAME', 'root')
 DB_PWD = os.environ.get('DB_PWD', 'root')
 DB_NAME = os.environ.get('DB_NAME', 'dev')
-
-
-LAMBDA_FUNCTION_NAME_FOR_CRON = get_lambda_name('get', 'executescheduledreport')
 
 GOOGLE_OAUTHCALLBACK_PATH = "/googleoauthcallback"
 GOOGLE_OAUTH_LOGIN = '/googleoauthlogin'
