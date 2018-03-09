5cfc7eea8115dfe0c2d7ec212d8635c50cc64f31
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-03-09 19:18:38 +0530

    click on groups number widget on dashboard should navigate to groups tree view

--- reactuiv2/src/components/Dashboard.js
+++ reactuiv2/src/components/Dashboard.js
@@ -30,7 +30,7 @@ class Dashboard extends Component {
     super();
     this.simpleWidgetConfigs = [
       { id: "usersCount", header: "Users", footer: "", renderType: "SimpleNumberWidget", link: "/users?header='Users'" },
-      { id: "groupsCount", header: "Groups", footer: "", renderType: "SimpleNumberWidget", link: "/users" },
+      { id: "groupsCount", header: "Groups", footer: "", renderType: "SimpleNumberWidget", link: "/users?header='Groups'" },
       { id: "filesCount", header: "Files", footer: "", renderType: "SimpleNumberWidget", link: "/resources?header='Files'" },
       { id: "foldersCount", header: "Folders", footer: "", renderType: "SimpleNumberWidget", link: "/resources?header='Folders'" },
     ];
--- reactuiv2/src/components/Users/Users.js
+++ reactuiv2/src/components/Users/Users.js
@@ -63,6 +63,11 @@ class Users extends Component {
       this.setState({
         showOnlyExternal: false
       })
+    else if (this.props.location.search.includes("Groups"))
+      this.setState({
+        showHierarchy: true,
+        showOnlyExternal: false
+      })
     this.props.onLoadStart();
     this.props.onLoad(agent.Users.getUsersTree());
   }
