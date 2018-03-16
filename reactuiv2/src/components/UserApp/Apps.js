6a8c42e988c1e2019cd24b209630de2e8186f434
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-03-16 12:05:37 +0530

    Removing the dependency from dashboard state for apps/users and checking for empty condition on component load

--- reactuiv2/src/components/UserApp/Apps.js
+++ reactuiv2/src/components/UserApp/Apps.js
@@ -11,14 +11,16 @@ import AppDetailsSection from './AppDetailsSection'
 import {
   APPS_PAGE_LOADED,
   APPS_PAGE_UNLOADED,
-  APPS_PAGE_LOAD_START
+  APPS_PAGE_LOAD_START,
+  ADD_APP_MESSAGE
 } from '../../constants/actionTypes';
 
 
 const mapStateToProps = state => ({
   appName: state.common.appName,
   currentApp: state.common.currentApp,
-  appsSearchPayload: state.apps.appsSearchPayload
+  appsSearchPayload: state.apps.appsSearchPayload,
+  appsPayload: state.apps.appPayLoad
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -28,6 +30,8 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: APPS_PAGE_UNLOADED }),
   onLoadStart: () =>
     dispatch({ type: APPS_PAGE_LOAD_START }),
+  flagAppsError: (error, info) => 
+    dispatch({ type: ADD_APP_MESSAGE, error, info })
 });
 
 // Here scopeExposure is 0 for all, 1 for readonly, 2 for fullyscope
@@ -35,7 +39,8 @@ class Apps extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        scopeExposure: 0
+        scopeExposure: 0,
+        appsEmpty: false
     }
     this.onCheckBoxChecked = this.onCheckBoxChecked.bind(this)
   }
@@ -49,9 +54,19 @@ class Apps extends Component {
     this.props.onLoadStart();
     this.props.onLoad(agent.Apps.getapps());
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.appsPayload && nextProps.appsPayload.length === 0 && !this.state.appsEmpty) {
+      this.props.flagAppsError("There are no apps for user to display", undefined)
+      this.setState({
+        appsEmpty: true
+      })
+    }
+  }
   
 
   onCheckBoxChecked = (e, { value }) => this.setState({ scopeExposure:value })
+  
   render() {
     //const { contextRef } = this.state
     let containerStyle = {
