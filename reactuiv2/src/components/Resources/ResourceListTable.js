3b718c03a5601dd57dc02b3882931e917969bdad
Amit Agarwal <amit@MacBook-Pro.local>
2018-03-19 18:17:34 +0530

    Making the reousrce table little compact. And also added all filtering input boxes for Abhra to implement :)

--- reactuiv2/src/components/Resources/ResourceListTable.js
+++ reactuiv2/src/components/Resources/ResourceListTable.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Loader, Dimmer, Button, Table, Dropdown, Form } from 'semantic-ui-react';
+import { Loader, Dimmer, Button, Table, Dropdown, Form, Input } from 'semantic-ui-react';
 
 import agent from '../../utils/agent';
-import DateComponent from '../DateComponent';
+import { IntlProvider, FormattedRelative } from 'react-intl';
 import ResourceSearch from '../Search/ResourceSearch'
 
 import {
@@ -45,64 +45,72 @@ class ResourcesListTable extends Component {
         }
 
         this.exposureFilterOptions = [
-            {text: 'Externally Shared',
-             value: 'EXT'},
-            {text: 'Domain Shared',
-             value: 'DOMAIN'},
-            {text: 'Internally Shared',
-             value: 'INT'},
-             {text: 'All Files',
-             value: 'ALL'}
-          ]
+            {
+                text: 'Externally Shared',
+                value: 'EXT'
+            },
+            {
+                text: 'Domain Shared',
+                value: 'DOMAIN'
+            },
+            {
+                text: 'Internally Shared',
+                value: 'INT'
+            },
+            {
+                text: 'All Files',
+                value: 'ALL'
+            }
+        ]
     }
 
     componentWillMount() {
         if (!this.props.pageNumber)
-            this.props.setPaginationData(0,100)
-        
+            this.props.setPaginationData(0, 100)
+
         this.props.onLoadStart()
-        this.props.onLoad(agent.Resources.getResourcesTree({'userEmails': [], 'exposureType': this.props.filterExposureType, 'resourceType': this.props.filterResourceType, 'pageNumber': 0, 'pageSize': 100}))
+        this.props.onLoad(agent.Resources.getResourcesTree({ 'userEmails': [], 'exposureType': this.props.filterExposureType, 'resourceType': this.props.filterResourceType, 'pageNumber': 0, 'pageSize': 100 }))
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps !== this.props) {
-            if ( nextProps.filterExposureType !== this.props.filterExposureType || nextProps.filterResourceType !== this.props.filterResourceType || 
-                 nextProps.pageNumber !== this.props.pageNumber ) {
+            if (nextProps.filterExposureType !== this.props.filterExposureType || nextProps.filterResourceType !== this.props.filterResourceType ||
+                nextProps.pageNumber !== this.props.pageNumber) {
                 nextProps.onLoadStart()
-                nextProps.onLoad(agent.Resources.getResourcesTree({'userEmails': [], 'exposureType': nextProps.filterExposureType, 'resourceType': nextProps.filterResourceType, 'pageNumber': nextProps.pageNumber, 'pageSize': nextProps.pageLimit}))
+                nextProps.onLoad(agent.Resources.getResourcesTree({ 'userEmails': [], 'exposureType': nextProps.filterExposureType, 'resourceType': nextProps.filterResourceType, 'pageNumber': nextProps.pageNumber, 'pageSize': nextProps.pageLimit }))
             }
         }
     }
 
-    handleClick = (event,rowData) => {
+    handleClick = (event, rowData) => {
         event.preventDefault()
         this.props.setRowData(rowData)
     }
 
-    handleExposureTypeChange = (event,data) => {
-        let value = data.value === 'ALL'?'':data.value
+    handleExposureTypeChange = (event, data) => {
+        let value = data.value === 'ALL' ? '' : data.value
         if (value !== this.props.filterExposureType)
-          this.props.changeFilter("filterExposureType", value);
+            this.props.changeFilter("filterExposureType", value);
     }
-    
+
     handleResourceTypeChange = (event) => {
         this.setState({
-          filterResourceType: event.target.value
+            filterResourceType: event.target.value
         });
     }
 
     handleKeyPress = (event) => {
         if (event.key === 'Enter') {
-          this.props.changeFilter("filterResourceType", this.state.filterResourceType);
+            this.props.changeFilter("filterResourceType", this.state.filterResourceType);
         }
     }
 
     handleNextClick = () => {
-        this.props.setPaginationData(this.props.pageNumber+1,this.props.pageLimit)
+        this.props.setPaginationData(this.props.pageNumber + 1, this.props.pageLimit)
     }
 
     handlePreviousClick = () => {
-        this.props.setPaginationData(this.props.pageNumber-1,this.props.pageLimit)
+        this.props.setPaginationData(this.props.pageNumber - 1, this.props.pageLimit)
     }
 
     render() {
@@ -119,17 +127,17 @@ class ResourcesListTable extends Component {
             resourceData = this.props.resourceSearchPayload
         else if (this.props.resourceTree)
             resourceData = this.props.resourceTree
-        
+
         if (resourceData)
             tableRowData = resourceData.map(rowData => {
                 return (
-                    <Table.Row onClick={(event) => this.handleClick(event,rowData)} style={this.props.rowData === rowData?{'background-color': '#ccccff'}:null}>
+                    <Table.Row onClick={(event) => this.handleClick(event, rowData)} style={this.props.rowData === rowData ? { 'background-color': '#ccccff' } : null}>
                         <Table.Cell>{rowData["resource_name"]}</Table.Cell>
                         <Table.Cell>{rowData["resource_type"]}</Table.Cell>
                         <Table.Cell>{rowData["resource_owner_id"]}</Table.Cell>
                         <Table.Cell textAlign="center">{rowData["exposure_type"]}</Table.Cell>
                         <Table.Cell>{rowData["parent_name"]}</Table.Cell>
-                        <Table.Cell><DateComponent value={rowData["last_modified_time"]} /></Table.Cell>
+                        <Table.Cell><IntlProvider locale='en'><FormattedRelative value={rowData["last_modified_time"]} /></IntlProvider ></Table.Cell>
                     </Table.Row>
                 )
             })
@@ -139,11 +147,11 @@ class ResourcesListTable extends Component {
                 <Loader inverted content='Loading' />
             </Dimmer>
         )
-        
+
         return (
             <div>
-                <div style={{'maxHeight': document.body.clientHeight, 'overflow': 'scroll', 'cursor': 'pointer' }}>
-                    <Table celled selectable striped>
+                <div style={{ 'maxHeight': document.body.clientHeight, 'overflow': 'auto', 'cursor': 'pointer' }}>
+                    <Table celled selectable striped compact='very' sortable>
                         <Table.Header>
                             <Table.Row>
                                 {tableHeaders}
@@ -155,32 +163,34 @@ class ResourcesListTable extends Component {
                                     <ResourceSearch />
                                 </Table.Cell>
                                 <Table.Cell>
-                                    <Form>
-                                        <Form.Field>
-                                        <input placeholder='Filter by File type ...' value={this.state.filterResourceType} onChange={this.handleResourceTypeChange} onKeyPress={this.handleKeyPress} />
-                                        </Form.Field>
-                                    </Form>
+                                    <Input placeholder='Filter by type...' value={this.state.filterResourceType} onChange={this.handleResourceTypeChange} onKeyPress={this.handleKeyPress} />
                                 </Table.Cell>
-                                <Table.Cell></Table.Cell>
+                                <Table.Cell>
+                                    <Input placeholder='Filter by email...' />
+                                </Table.Cell>
                                 <Table.Cell>
                                     <Dropdown
                                         options={this.exposureFilterOptions}
                                         selection
-                                        value={this.props.filterExposureType === ''?'ALL':this.props.filterExposureType}
+                                        value={this.props.filterExposureType === '' ? 'ALL' : this.props.filterExposureType}
                                         onChange={this.handleExposureTypeChange}
                                     />
                                 </Table.Cell>
-                                <Table.Cell></Table.Cell>
-                                <Table.Cell></Table.Cell>
+                                <Table.Cell>
+                                    <Input placeholder='Filter by folder...' />
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <Input placeholder='Filter by date...' />
+                                </Table.Cell>
                             </Table.Row>
                             {tableRowData}
                         </Table.Body>
-                        {this.props.isLoading?dimmer:null}    
+                        {this.props.isLoading ? dimmer : null}
                     </Table>
                 </div>
                 <div style={{ marginTop: '5px' }} >
-                    {(this.props.resourceTree && this.props.resourceTree.length < this.props.pageLimit)||(this.props.isLoading)?null:(<Button color='green' size="mini" style={{float: 'right', width: '80px'}} onClick={this.handleNextClick} >Next</Button>)}
-                    {this.props.pageNumber !== 0?(<Button color='green' size="mini" style={{float: 'right', width: '80px'}} onClick={this.handlePreviousClick} >Previous</Button>):null}
+                    {(this.props.resourceTree && this.props.resourceTree.length < this.props.pageLimit) || (this.props.isLoading) ? null : (<Button color='green' size="mini" style={{ float: 'right', width: '80px' }} onClick={this.handleNextClick} >Next</Button>)}
+                    {this.props.pageNumber !== 0 ? (<Button color='green' size="mini" style={{ float: 'right', width: '80px' }} onClick={this.handlePreviousClick} >Previous</Button>) : null}
                 </div>
             </div>
         )
