c3bc529bcac1022249e29aff5b2fd42e70b0cbac
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-03-20 11:06:01 +0530

    Fixing the Resource page loading from Dashboard

--- reactuiv2/src/components/Resources/Resources.js
+++ reactuiv2/src/components/Resources/Resources.js
@@ -16,7 +16,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  //changeFilter: (property, value) => dispatch({ type: RESOURCES_FILTER_CHANGE, property, value })
+  changeFilter: (property, value) => dispatch({ type: RESOURCES_FILTER_CHANGE, property, value })
 });
 
 
