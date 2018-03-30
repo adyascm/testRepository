e9883c63a3261bb49dc76312ad4faf87fd1047f7
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-03-30 16:38:36 +0530

    Adding and commenting the handleSubmit action for Policy; Work in progress

--- reactuiv2/src/reducers/policy.js
+++ reactuiv2/src/reducers/policy.js
@@ -1,14 +1,44 @@
 import {
-    SET_POLICY_ROW_FILTERS
+    SET_POLICY_ROW_FILTERS,
+    SET_POLICY_FILTER,
+    CREATE_POLICY_LOAD_START,
+    CREATE_POLICY_LOADED
 } from '../constants/actionTypes'
 
+const defaultState = {
+    filterType: [],
+    filterCondition: [],
+    filterValue: [],
+    actionType: ''
+}
 
-export default (state={}, action) => {
+export default (state=defaultState, action) => {
     switch(action.type) {
         case SET_POLICY_ROW_FILTERS:
             return {
                 ...state,
                 policyFilters: action.payload
+            }
+        case SET_POLICY_FILTER:
+            if (action.policyFilterType === 'filterType' || 
+                action.policyFilterType === 'filterCondition' ||
+                action.policyFilterType === 'filterValue')
+                    state[action.policyFilterType].push(action.policyFilterValue)
+            else 
+                state[action.policyFilterType] = action.policyFilterValue
+            return {
+                ...state
+            }
+        case CREATE_POLICY_LOAD_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case CREATE_POLICY_LOADED:
+            return {
+                ...state,
+                policyData: !action.error?action.payload:[],
+                isLoading: false
             }
         default:
             return {
