e1f615f3db51f83b46ce0b53e369f232e3a857b3
Amit Agarwal <amit.agarwal@adya.io>
2018-04-15 11:16:53 +0530

    Removing the useDomainAccess check, not sure how this is related to team drive

--- apiv2/adya/gsuite/actions.py
+++ apiv2/adya/gsuite/actions.py
@@ -134,7 +134,7 @@ class AddOrUpdatePermisssionForResource():
                 "emailAddress": email
 
             }
-            request = self.drive_service.permissions().create(fileId=resource_id, body=add_permission_object, useDomainAdminAccess=True,
+            request = self.drive_service.permissions().create(fileId=resource_id, body=add_permission_object,
                                                               transferOwnership=True if role == 'owner' else False,
                                                               fields='id, emailAddress, type, kind, displayName')
             try:
@@ -187,7 +187,7 @@ class AddOrUpdatePermisssionForResource():
                 "role": role,
             }
             request = self.drive_service.permissions().update(fileId=resource_id, body=update_permission_object,
-                                                              permissionId=permission_id, useDomainAdminAccess=True,
+                                                              permissionId=permission_id,
                                                               transferOwnership=True if role == 'owner' else False)
             self.change_requests.append(request)
         self.execute()
@@ -201,7 +201,7 @@ class AddOrUpdatePermisssionForResource():
         for permission in self.permissions:
             resource_id = permission['resource_id']
             permission_id = permission['permission_id']
-            request = self.drive_service.permissions().delete(fileId=resource_id, permissionId=permission_id, useDomainAdminAccess=True)
+            request = self.drive_service.permissions().delete(fileId=resource_id, permissionId=permission_id)
             self.change_requests.append(request)
         self.execute()
         delete_resource_permission(self.initiated_by_email, self.datasource_id, self.updated_permissions)
