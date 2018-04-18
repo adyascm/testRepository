6c752466f59424bd0ecb1a462be0c2d241170c92
adyaabhi <abhishek.kumar@adya.io>
2018-04-18 16:03:03 +0530

    issue#163: 0 or more ui fix in listwidget

--- reactuiv2/src/components/Widgets/ListWidget.js
+++ reactuiv2/src/components/Widgets/ListWidget.js
@@ -36,7 +36,7 @@ class ListWidget extends Component {
                 const data = this.props[this.props.config.id].data.rows;
                 const count = this.props[this.props.config.id].data.totalCount;
                 const remainingCount = count - data.length
-                const footer = count >= 5?"...and " + remainingCount + " more":null;                
+                const footer = count > 5 ? "...and " + remainingCount + " more":null;
                 
                 if (!count)
                     return null
