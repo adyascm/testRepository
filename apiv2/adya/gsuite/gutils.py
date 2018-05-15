20e5b21520e20bba9baf165ad76e84447833073b
Rashmi Singh <rashmi.singh@adya.io>
2018-05-15 19:45:59 +0530

    separated the create datasource for gsuite and slack

--- apiv2/adya/gsuite/gutils.py
+++ apiv2/adya/gsuite/gutils.py
@@ -1,16 +1,23 @@
+import csv
+import uuid
+
+import datetime
 from google.oauth2.credentials import Credentials
 import googleapiclient.discovery as discovery
 import json
 import requests
+from sqlalchemy import Boolean
 
-from adya.common.constants import constants
+from adya.common.constants import constants, urls
 from adya.common.db.connection import db_connection
-from adya.common.db.models import LoginUser, Domain
+from adya.common.db.models import LoginUser, Domain, DataSource, get_table, Resource, DomainGroup, DomainUser
 from oauth2client.service_account import ServiceAccountCredentials
 from adya.common.constants import constants
 from adya.common.constants.scopeconstants import DRIVE_SCAN_SCOPE, SERVICE_ACCOUNT_SCOPE, SERVICE_ACCOUNT_READONLY_SCOPE
 import os
 import httplib2
+
+from adya.common.utils import messaging
 from adya.common.utils.response_messages import Logger
 
 GOOGLE_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
@@ -159,3 +166,116 @@ def get_resource_exposure_type(permission_exposure, highest_exposure):
     elif permission_exposure == constants.ResourceExposureType.INTERNAL and not (highest_exposure == constants.ResourceExposureType.PUBLIC or highest_exposure == constants.ResourceExposureType.ANYONEWITHLINK or highest_exposure == constants.ResourceExposureType.EXTERNAL or highest_exposure == constants.ResourceExposureType.DOMAIN):
         highest_exposure = constants.ResourceExposureType.INTERNAL
     return highest_exposure
+
+
+def create_datasource(auth_token, db_session, existing_user, payload):
+    datasource_id = str(uuid.uuid4())
+    datasource = DataSource()
+    datasource.domain_id = existing_user.domain_id
+    datasource.datasource_id = datasource_id
+    datasource.is_dummy_datasource = True if payload.get(
+        "isDummyDatasource") else False
+
+    if payload.get("display_name"):
+        datasource.display_name = payload["display_name"]
+    else:
+        datasource.display_name = "Unnamed datasource"
+    # we are fixing the datasoure type this can be obtained from the frontend
+    datasource.datasource_type = payload ["datasource_type"]
+    datasource.creation_time = datetime.datetime.utcnow()
+    if datasource.is_dummy_datasource:
+        datasource.is_serviceaccount_enabled = False
+    else:
+        datasource.is_serviceaccount_enabled = existing_user.is_serviceaccount_enabled
+
+    is_admin_user = check_if_user_isamdin(auth_token, existing_user.email, db_session)
+
+    # If service account is enabled, non admin cannot create a data source
+    if (datasource.is_serviceaccount_enabled and not is_admin_user):
+        raise Exception(
+            "Action not allowed, please contact your administrator...")
+
+    if not is_admin_user:
+        datasource.user_scan_status = 1
+        datasource.group_scan_status = 1
+
+    if is_admin_user and not datasource.is_serviceaccount_enabled:
+        # Since it is an admin user, update the domain name in domain table to strip off the full email
+        domain_name = get_domain_name_from_email(existing_user.email)
+        db_session.query(Domain).filter(Domain.domain_id == existing_user.domain_id).update(
+            {"domain_name": domain_name})
+
+    db_session.add(datasource)
+    db_connection().commit()
+    if datasource.is_dummy_datasource:
+        create_dummy_datasource(db_session, existing_user.domain_id, datasource_id)
+
+    else:
+        Logger().info("Starting the scan")
+        query_params = {"isAdmin": str(is_admin_user), "domainId": datasource.domain_id,
+                        "dataSourceId": datasource.datasource_id,
+                        "serviceAccountEnabled": str(datasource.is_serviceaccount_enabled)}
+        messaging.trigger_post_event(urls.SCAN_START, auth_token, query_params, {}, "gsuite")
+        print "Received the response of start scan api"
+    return datasource
+
+
+def create_dummy_datasource(db_session, domain_id, datasource_id):
+    file_names = ['resource', 'user', 'group',
+                  'directory_structure', 'resource_permission', 'application', 'app_user_association']
+    for filename in file_names:
+        results = []
+        with open(dir_path + "/dummy_datasource/" + filename + ".csv") as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            tablename = get_table(filename)
+            columns = tablename.__table__.columns
+            firstrow = True
+            for row in csvReader:
+                if firstrow:
+                    firstrow = False
+                    continue
+                datarow = {}
+                for cellvalue, column in zip(row, columns):
+                    column_name = column.name
+                    column_type = column.type
+                    if cellvalue == 'NULL' or cellvalue == '':
+                        datarow[column_name] = None
+                    elif isinstance(column_type, Boolean):
+                        if cellvalue == '0':
+                            datarow[column_name] = False
+                        else:
+                            datarow[column_name] = True
+                    elif column_name == 'domain_id':
+                        datarow[column_name] = domain_id
+                    elif column_name == 'datasource_id':
+                        datarow[column_name] = datasource_id
+                    else:
+                        datarow[column_name] = cellvalue
+                results.append(datarow)
+        db_session.bulk_insert_mappings(tablename, results)
+    db_connection().commit()
+    update_datasource_column_count(db_session, domain_id, datasource_id)
+
+
+def update_datasource_column_count(db_session, domain_id, datasource_id):
+    datasouorce = db_session.query(DataSource).filter(DataSource.datasource_id == datasource_id).first()
+    filecount = db_session.query(Resource.resource_id).distinct(Resource.resource_id).\
+        filter(Resource.datasource_id == datasource_id).count()
+    group_count = db_session.query(DomainGroup).distinct(DomainGroup.group_id).\
+        filter(DomainGroup.datasource_id == datasource_id).count()
+    user_count = db_session.query(DomainUser).distinct(DomainUser.user_id).\
+        filter(DomainUser.datasource_id == datasource_id).count()
+    datasouorce.total_file_count = filecount
+    datasouorce.processed_file_count = filecount
+    datasouorce.file_scan_status = user_count
+
+    datasouorce.total_group_count = group_count
+    datasouorce.processed_group_count = group_count
+    datasouorce.group_scan_status = 1
+
+    datasouorce.total_user_count = user_count
+    datasouorce.processed_user_count = user_count
+    datasouorce.user_scan_status = 1
+
+    db_session.add(datasouorce)
+    db_connection().commit()
--- apiv2/adya/slack/slack_utils.py
+++ apiv2/adya/slack/slack_utils.py
@@ -1,11 +1,17 @@
+import uuid
+
+import datetime
+
+from adya.common.constants import urls
 from adya.common.db.connection import db_connection
 
-from adya.common.db.models import LoginUser
+from adya.common.db.models import LoginUser, DataSource
 from slackclient import SlackClient
+
+from adya.common.utils import messaging
 
 
 def get_slack_client(authtoken):
-
     db_session = db_connection().get_session()
     login_user_info = db_session.query(LoginUser).filter(LoginUser.auth_token == authtoken).first()
 
@@ -14,3 +20,38 @@ def get_slack_client(authtoken):
     sc = SlackClient(access_token)
 
     return sc
+
+
+def check_for_admin_user(authtoken, user_id):
+    slack_client = get_slack_client(authtoken)
+    user_info = slack_client.api_call(
+        "users.info",
+        user=user_id
+    )
+
+    is_admin = user_info['is_admin']
+    return is_admin
+
+
+def create_datasource(auth_token, db_session, existing_user, payload):
+    datasource_id = str(uuid.uuid4())
+    datasource = DataSource()
+    datasource.domain_id = existing_user.domain_id
+    datasource.datasource_id = datasource_id
+
+    if payload.get("display_name"):
+        datasource.display_name = payload["display_name"]
+    else:
+        datasource.display_name = "Unnamed datasource"
+
+    datasource.creation_time = datetime.datetime.utcnow()
+    datasource.datasource_type = payload["datasource_type"]
+
+    db_session.add(datasource)
+    db_connection().commit()
+
+    query_params = {"domainId": datasource.domain_id,
+                    "dataSourceId": datasource.datasource_id,
+                   }
+    messaging.trigger_post_event(urls.SCAN_SLACK_START, auth_token, query_params, {}, "slack")
+    return datasource
