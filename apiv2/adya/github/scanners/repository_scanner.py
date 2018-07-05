a9a5f9f96036e87477c69e3a9de78aad849943ef
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-07-05 13:02:50 +0530

    Action remove_all_external_users_from_repository added for github; Also corrected the widgets count for files and groups; Updated the resource permission table entries for repositories

--- apiv2/adya/github/scanners/repository_scanner.py
+++ apiv2/adya/github/scanners/repository_scanner.py
@@ -15,12 +15,14 @@ def query(auth_token, query_params, scanner):
     github_client = github_utils.get_github_client(datasource_id)
     all_collaborators = []
     collaborators_list = []
+    collaborator_permissions_dict = {}
     repo = github_client.get_repo(repo_name)
     for collaborator in repo.get_collaborators():
         if collaborator not in all_collaborators:
             print collaborator.name
             all_collaborators.append(collaborator)
             collaborators_list.append(collaborator.raw_data)
+            collaborator_permissions_dict[collaborator.id] = collaborator.permissions.raw_data
     
     #Priting the hooks associated with the repository
     try:
@@ -29,13 +31,15 @@ def query(auth_token, query_params, scanner):
     except Exception as ex:
         print ex
     
-    return {"payload": collaborators_list}
+    return {"payload": collaborators_list, "permissions": collaborator_permissions_dict}
 
 def process(db_session, auth_token, query_params, scanner_data):
     collaborators_list = scanner_data["entities"]
     datasource_id = query_params["datasource_id"]
     scanner_id = query_params["scanner_id"]
     domain_id = query_params["domain_id"]
+    repo_id = query_params["repo_id"]
+    collaborators_permissions_dict = scanner_data["permissions"]
     all_collaborators = []
     processed_collaborators_count = 0
 
@@ -44,6 +48,9 @@ def process(db_session, auth_token, query_params, scanner_data):
 
     # print existing_collaborators
     
+    repo_permission_list = []
+    resource_exposure = None
+
     for collaborator in collaborators_list:
         collaborator_info = {}
         collaborator_info["datasource_id"] = datasource_id
@@ -68,13 +75,50 @@ def process(db_session, auth_token, query_params, scanner_data):
         if github_utils.is_external_user(domain_id, collaborator_info["email"]):
             collaborator_info["member_type"] = constants.EntityExposureType.EXTERNAL.value
         
+        #Update the Resource permissions table for the current repository
+        repo_permission_dict = {}
+        repo_permission_dict["datasource_id"] = datasource_id
+        repo_permission_dict["resource_id"] = repo_id
+        repo_permission_dict["email"] = collaborator_info["email"]
+        repo_permission_dict["permission_id"] = collaborator["id"]
+        repo_permission_dict["exposure_type"] = collaborator_info["member_type"]
+
+        if collaborator["id"] in collaborators_permissions_dict:
+            collaborator_permission = collaborators_permissions_dict[collaborator["id"]]
+            if collaborator_permission["admin"]:
+                repo_permission_dict["permission_type"] = constants.Role.OWNER.value
+            elif collaborator_permission["push"]:
+                repo_permission_dict["permission_type"] = constants.Role.WRITER.value
+            else:
+                repo_permission_dict["permission_type"] = constants.Role.READER.value
+
+        if collaborator_info["member_type"] == constants.EntityExposureType.INTERNAL.value:
+            if not resource_exposure:
+                resource_exposure = constants.EntityExposureType.INTERNAL.value
+        elif collaborator_info["member_type"] == constants.EntityExposureType.EXTERNAL.value:
+            resource_exposure = constants.EntityExposureType.EXTERNAL.value
+
+        repo_permission_list.append(repo_permission_dict)
         all_collaborators.append(collaborator_info)
         processed_collaborators_count = processed_collaborators_count + 1
 
     try:
         #db_session.bulk_insert_mappings(DomainUser, all_collaborators)
         db_session.execute(DomainUser.__table__.insert().prefix_with("IGNORE").values(all_collaborators))
+        db_session.execute(ResourcePermission.__table__.insert().prefix_with("IGNORE").values(repo_permission_list))
         db_connection().commit()
+        
+        #Updating the resource exposure in Resource table
+        while (True):
+            try:
+                db_session.query(Resource).filter(Resource.datasource_id == datasource_id, Resource.resource_id == repo_id). \
+                    update({ Resource.exposure_type: resource_exposure })
+                db_connection().commit()
+                break
+            except Exception as ex:
+                print ex
+                db_session.rollback()
+        
         return processed_collaborators_count
     
     except Exception as ex:
