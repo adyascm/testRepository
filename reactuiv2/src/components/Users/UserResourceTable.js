bc85cd6b1cba1ecc853722226504e8435d10d041
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-07-12 13:17:21 +0530

    Adding fixed table property to users owned/accessible docs table

--- reactuiv2/src/components/Users/UserResourceTable.js
+++ reactuiv2/src/components/Users/UserResourceTable.js
@@ -117,16 +117,16 @@ class UserResourceTable extends Component {
             tableRowData = this.props.selectedUserItem.resources.map((rowData, index) => {
                 if (ds.datasource_type != "GSUITE") {
                     return(<Table.Row key={index}>
-                        <Table.Cell width='5' style={{ 'wordBreak': 'break-all' }}>{rowData["resource_name"]}</Table.Cell>
-                        <Table.Cell width='4'>{rowData["resource_owner_id"]}</Table.Cell>
+                        <Table.Cell style={{ 'wordBreak': 'break-all' }}>{rowData["resource_name"]}</Table.Cell>
+                        <Table.Cell >{rowData["resource_owner_id"]}</Table.Cell>
                         <Table.Cell>{rowData["exposure_type"]}</Table.Cell>
                         <Table.Cell><Label as='a' color='blue' active onClick={openLink(rowData["web_view_link"])}>View</Label></Table.Cell>
                     </Table.Row>);
                 }
                 return (
                     <Table.Row key={index}>
-                        <Table.Cell width='5' style={{ 'wordBreak': 'break-all' }}>{rowData["resource_name"]}</Table.Cell>
-                        <Table.Cell width='4'>{rowData["resource_owner_id"]}</Table.Cell>
+                        <Table.Cell style={{ 'wordBreak': 'break-all' }}>{rowData["resource_name"]}</Table.Cell>
+                        <Table.Cell>{rowData["resource_owner_id"]}</Table.Cell>
                         <Table.Cell textAlign="center" width='3'>
                             <Dropdown fluid selection options={this.state.permissionOptions} value={rowData["myPermission"]} onChange={(event, data) => this.onPermissionChange(event, rowData, data.value)} />
                         </Table.Cell>
@@ -149,7 +149,7 @@ class UserResourceTable extends Component {
                 return (
                     <div>
                         <div>
-                            <Table celled selectable striped compact="very">
+                            <Table celled selectable striped fixed>
                                 <Table.Header>
                                     <Table.Row>
                                         {tableHeaders}
