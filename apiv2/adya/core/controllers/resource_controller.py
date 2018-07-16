b14f73b366e786184ac60de17361266e429df727
Abhra Dey <abhradey@Abhras-MacBook-Air.local>
2018-07-17 01:15:54 +0530

    Refactoring the export to csv code for resources

--- apiv2/adya/core/controllers/resource_controller.py
+++ apiv2/adya/core/controllers/resource_controller.py
@@ -5,6 +5,7 @@ from adya.common.db.connection import db_connection
 from adya.common.db import db_utils
 from adya.common.db.models import Resource,ResourcePermission,LoginUser,DataSource,ResourcePermission,ResourceParent,Domain, DomainUser
 from adya.common.constants import constants
+from adya.common.utils import utils, aws_utils
 from boto3.s3.transfer import S3Transfer
 from datetime import datetime
 import csv, boto3, os, tempfile
@@ -12,14 +13,9 @@ import csv, boto3, os, tempfile
 current_file_path = os.path.dirname(os.path.realpath(__file__))
 export_csv_dir = current_file_path + '/../../common/csv_exports'
 
-def get_resources(auth_token, page_number, page_limit, accessible_by=None, exposure_type='EXT', resource_type='None', prefix='',
+def fetch_filtered_resources(db_session, auth_token, accessible_by=None, exposure_type='EXT', resource_type='None', prefix='',
                   owner_email_id=None, parent_folder=None, selected_date=None, sort_column_name=None, sort_type=None, datasource_id=None, source_type=None):
-    if not auth_token:
-        return None
-    page_number = page_number if page_number else 0
-    page_limit = page_limit if page_limit else constants.PAGE_LIMIT
-
-    db_session = db_connection().get_session()
+    #db_session = db_connection().get_session()
     existing_user = db_utils.get_user_session(auth_token)
     user_domain_id = existing_user.domain_id
     loggged_in_user_email = existing_user.email
@@ -30,10 +26,11 @@ def get_resources(auth_token, page_number, page_limit, accessible_by=None, expos
     else:
         domain_datasource_ids = db_session.query(DataSource.datasource_id).filter(DataSource.domain_id == user_domain_id).all()
         domain_datasource_ids = [r for r, in domain_datasource_ids]
-    resources = []
+    # resources = []
     resource_alias = aliased(Resource)
     parent_alias = aliased(Resource)
     resources_query = db_session.query(resource_alias, parent_alias.resource_name).outerjoin(parent_alias, and_(resource_alias.parent_id == parent_alias.resource_id, resource_alias.datasource_id == parent_alias.datasource_id))
+
     if source_type:
         resources_query = resources_query.filter(resource_alias.datasource_id == source_type)
     if accessible_by and not owner_email_id:
@@ -87,7 +84,21 @@ def get_resources(auth_token, page_number, page_limit, accessible_by=None, expos
         else:
             sort_column_obj = sort_column_obj.desc()
         resources_query = resources_query.order_by(sort_column_obj)
+    
+    return resources_query, resource_alias, parent_alias
 
+def get_resources(auth_token, page_number, page_limit, accessible_by=None, exposure_type='EXT', resource_type='None', prefix='',
+                  owner_email_id=None, parent_folder=None, selected_date=None, sort_column_name=None, sort_type=None, datasource_id=None, source_type=None):
+    if not auth_token:
+        return None
+    
+    db_session = db_connection().get_session()
+    page_number = page_number if page_number else 0
+    page_limit = page_limit if page_limit else constants.PAGE_LIMIT
+
+    #Code moved to seperate common method fetch_filtered_resources
+    resources_query, resource_alias, parent_alias = fetch_filtered_resources(db_session, auth_token, accessible_by, exposure_type, resource_type, prefix,
+                  owner_email_id, parent_folder, selected_date, sort_column_name, sort_type, datasource_id, source_type)
     resources = resources_query.offset(page_number * page_limit).limit(page_limit).all()
     result = []
     for resource in resources:
@@ -121,95 +132,34 @@ def export_to_csv(auth_token, payload):
     db_session = db_connection().get_session()
     existing_user = db_utils.get_user_session(auth_token)
     domain_id = existing_user.domain_id
-    datasources = db_session.query(DataSource).filter(DataSource.domain_id == domain_id).all()
-    domain_datasource_ids = []
-    for datasource in datasources:
-        domain_datasource_ids.append(datasource.datasource_id)
-    resource_alias = aliased(Resource)
-    parent_alias = aliased(Resource)
-    #resources_query = db_session.query(Resource).filter(Resource.datasource_id == datasource.datasource_id)
-    resources_query = db_session.query(resource_alias, parent_alias.resource_name).outerjoin(parent_alias, and_(resource_alias.parent_id == parent_alias.resource_id, resource_alias.datasource_id == parent_alias.datasource_id, resource_alias.datasource_id.in_(domain_datasource_ids)))
+
+    resources_query, resource_alias, parent_alias = fetch_filtered_resources(db_session, auth_token, exposure_type=exposure_type, resource_type=type, prefix=name,
+        owner_email_id=owner, parent_folder=parent_folder, selected_date=modified_date, source_type=source)
 
     column_fields = []
 
     if source is not None:
-        column_fields.append(resource_alias.datasource_id)
-        if source:
-            resources_query = resources_query.filter(resource_alias.datasource_id == source)
+        column_fields.append(DataSource.datasource_type)
     if name is not None:
         column_fields.append(resource_alias.resource_name)
-        if name:
-            resources_query = resources_query.filter(resource_alias.resource_name.ilike("%" + name + "%"))
     if type is not None:
         column_fields.append(resource_alias.resource_type)
-        if type:
-            resources_query = resources_query.filter(resource_alias.resource_type == type)
     if owner is not None:
         column_fields.append(resource_alias.resource_owner_id)
-        if owner:
-            resources_query = resources_query.filter(resource_alias.resource_owner_id.ilike("%" + owner + "%"))
     if exposure_type is not None:
         column_fields.append(resource_alias.exposure_type)
-        if exposure_type:
-            resources_query = resources_query.filter(resource_alias.exposure_type == exposure_type)
     if parent_folder is not None:
         column_fields.append(parent_alias.resource_name)
-        if parent_folder:
-            resources_query = resources_query.filter(parent_alias.resource_name == parent_folder)
     if modified_date is not None:
         column_fields.append(resource_alias.last_modified_time)
-        if modified_date:
-            resources_query = resources_query.filter(resource_alias.last_modified_time <= modified_date)
 
-    resources = resources_query.with_entities(*column_fields).all()
+    resources = resources_query.with_entities(*column_fields).filter(DataSource.datasource_id == resource_alias.datasource_id).all()
     print resources
 
-    #Creating a tempfile for csv data
-    with tempfile.NamedTemporaryFile() as temp_csv:
-        csv_writer = csv.writer(temp_csv)
-        csv_writer.writerows(resources)
-        temp_csv.seek(0)
-
-        #Reading the tempfile created
-        csv_reader = csv.reader(temp_csv)
-        for row in csv_reader:
-            print row
-        #temp_csv.close()
-
-        #Uploading the file in s3 bucket
-        client = boto3.client('s3', aws_access_key_id=constants.ACCESS_KEY_ID, aws_secret_access_key=constants.SECRET_ACCESS_KEY)
-        bucket_name = "adya-app-" + constants.DEPLOYMENT_ENV + "-data"
-        bucket_obj = None
-        try:
-            bucket_obj = client.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={
-                    'LocationConstraint': 'ap-south-1'
-                })
-            print bucket_obj
-        
-        except Exception as ex:
-            print ex
-            err_response = ex.response
-            if err_response['Error']['Code'] == "BucketAlreadyOwnedByYou":
-                print "Bucket already created!!"
-        
-        transfer = S3Transfer(client)
-        now = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")
-        #now = str(datetime.now())
-        key = domain_id + "/export/resource-" + now
-        transfer.upload_file(temp_csv.name, bucket_name, key)
-        
-        #Constructing a temporary file url 
-        temp_url = client.generate_presigned_url(
-            'get_object', 
-            Params = {
-                'Bucket': bucket_name,
-                'Key': key,
-            },
-            ExpiresIn=60)
-        print temp_url
-        return temp_url
-
-
-    
+    temp_csv = utils.convert_data_to_csv(resources)
+    bucket_name = "adya-app-" + constants.DEPLOYMENT_ENV + "-data"
+    now = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")
+    #now = str(datetime.now())
+    key = domain_id + "/export/resource-" + now
+    temp_url = aws_utils.upload_file_in_s3_bucket(bucket_name, key, temp_csv)
+    return temp_url
